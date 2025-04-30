8acbc9cbdddb6961d94fd228c5be39df03c37223
Vahid <VJalili@users.noreply.github.com>
2025-04-30 10:14:28 -0400

    Add HailMerge WDL and scripts (#1)
    * Add hail merge scripts.
    
    * Filename fix.
    
    * Add comment about using correct docker images.

---
+++ scripts/sv-pipeline/reset_cnv_gts.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+# Hail and and its dependencies were included in GATK-SV docker images until version v1.0.3.
+# To run this code, please make sure you're using a Docker images that has Hail and all its dependencies installed.
+
+"""
+Sets CNV GT fields to "."
+This is needed following any HailMerge step for VCFs containing CNVs
+"""
+
+import argparse
+import sys
+import pysam
+
+
+def reset_cnv_gts(vcf, fout):
+
+    for record in vcf:
+        if record.info['SVTYPE'] == 'CNV':
+            for sample in record.samples:
+                record.samples[sample]['GT'] = (None,)
+        fout.write(record)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('vcf')
+    parser.add_argument('fout')
+
+    args = parser.parse_args()
+
+    if args.vcf in '- stdin'.split():
+        vcf = pysam.VariantFile(sys.stdin)
+    else:
+        vcf = pysam.VariantFile(args.vcf)
+
+    header = vcf.header
+
+    if args.fout in '- stdout'.split():
+        fout = pysam.VariantFile(sys.stdout, 'w', header=header)
+    else:
+        fout = pysam.VariantFile(args.fout, 'w', header=header)
+
+    reset_cnv_gts(vcf, fout)
+
+
+if __name__ == '__main__':
+    main()
